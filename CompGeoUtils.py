797fddf628f73c7c593fc3da18b60182c05a2e5b
Isaac <robertsisaacp@gmail.com>
2017-09-21 14:07:39 -0400

    added comments, revised arg consistency across 3pt functions

--- CompGeoUtils.py
+++ CompGeoUtils.py
@@ -4,36 +4,35 @@ import matplotlib.pyplot as plt
 
 def to_np_array(filename: str):
     with open(filename, 'rt') as f:
-        tempPoints = []
+        temp_points = []
         for lineNumber, line in enumerate(f):
             if lineNumber != 0:
                 x, y = line.split()
-                tempPoints.append((float(x), float(y)))
-    return np.array(tempPoints)
+                temp_points.append((float(x), float(y)))
+    return np.array(temp_points)
 
 
-def left_of(a, b, c, Print=False):
-    return area_of_triangle(a, b, c, Print) > 0
+def left_of(p0, p1, p2):
+    # is p2 to the left of line p0p1?
+    return area_of_triangle(p0, p1, p2) > 0
 
 
-def right_of(a, b, c, Print=False):
-    return area_of_triangle(a, b, c, Print) < 0
+def right_of(p0, p1, p2):
+    # is p2 to the right of line p0p1?
+    return area_of_triangle(p0, p1, p2) < 0
 
 
-def area_of_triangle(p1, p2, p3, Print=False):
-    # takes np.array points
-    if Print:
-        print("" + str(p1) + "," + str(p2) + "," + str(p3) + ":" + str(np.cross((p2 - p1), (p3 - p1)) / 2.))
-    return np.cross((p2 - p1), (p3 - p1)) / 2.
+def area_of_triangle(p0, p1, p2):
+    return np.cross((p1 - p0), (p2 - p0)) / 2.
 
 
 def draw_hull(hull):
     for p, p_next in zip(hull, hull[1:]):
-        print_points(hull, 'ro', 1)
+        plot_points(hull, 'ro', 1)
         plt.plot([p[0], p_next[0]], [p[1], p_next[1]], 'r--')
 
+    # account for last pair of points
     if len(hull) > 2:
-        pass
         plt.plot([hull[-1][0], hull[0][0]], [hull[-1][1], hull[0][1]], 'r--')
 
 
@@ -85,11 +84,10 @@ def convex_hull(points):
     # split hull in half based on extrema, recursively call find_hull
     find_hull(right_pts, hull[0], hull[1])
     find_hull(left_pts, hull[-1], hull[0])
-    print_points(points, 'bo')
+    plot_points(points, 'bo')
     draw_hull(hull)
 
 
-def print_points(points, color='ro', z=0):
+def plot_points(points, color='ro', z=0):
     x, y = zip(*points)
     plt.plot(x, y, color, zorder=z)
-
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from CompGeoUtils import *
 
 
 # Generate or extract points
-# arr = to_np_array('input.txt')
-arr = np.array([*zip(np.random.randint(0, 200, 30),
-                     np.random.randint(0, 200, 30))])
+arr = to_np_array('input.txt')
+#arr = np.array([*zip(np.random.randint(0, 200, 30),
+#                     np.random.randint(0, 200, 30))])
 
 # Compute Convex Hull
 convex_hull(arr)
